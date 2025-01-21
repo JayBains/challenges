de5736b32be0630462d4ba0cf196db1dbec4daf0
JayBains <jayarjunbains@gmail.com>
2025-01-21 10:45:42 +0000

    Completed render new element challenge

---
+++ ts/Render new elements with object key values/src/main.ts
@@ -0,0 +1,33 @@
+import { companies } from "./data";
+console.log(companies);
+
+const button = document.getElementById("render-companies") as HTMLElement;
+const cardContainer = document.getElementById(
+  "companies-container"
+) as HTMLElement;
+
+const createCard = () => {
+  cardContainer.innerHTML = "";
+
+  companies.forEach((element) => {
+    const card = document.createElement("div");
+    const name = document.createElement("h3");
+    const industry = document.createElement("p");
+    const start = document.createElement("p");
+    const end = document.createElement("p");
+
+    name.appendChild(document.createTextNode(`Company name: ${element.name}`));
+    industry.appendChild(document.createTextNode(`Ind: ${element.industry}`));
+    start.appendChild(document.createTextNode(`Start: ${element.start}`));
+    end.appendChild(document.createTextNode(`End: ${element.end}`));
+
+    card.appendChild(name);
+    card.appendChild(industry);
+    card.appendChild(start);
+    card.appendChild(end);
+
+    cardContainer.appendChild(card);
+  });
+};
+button.addEventListener("click", createCard);
+console.log(button);
