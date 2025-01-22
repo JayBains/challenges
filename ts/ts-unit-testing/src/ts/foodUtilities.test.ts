b5fd3a23eb2b351d984e6c1d6cf5ceb118400522
JayBains <jayarjunbains@gmail.com>
2025-01-22 14:44:08 +0000

    Added tests for correct food type

--- ts/ts-unit-testing/src/ts/foodUtilities.test.ts
+++ ts/ts-unit-testing/src/ts/foodUtilities.test.ts
@@ -1,3 +1,5 @@
+import { FoodType } from "./Food";
+import foodStock from "./foodData";
 import {
   capitalizeString,
   filterFoodByType,
@@ -19,5 +21,38 @@ describe("Testing capitalizeString", () => {
     expect(output).toBe("Croissant");
     // use .toBe to compare primitive values or to check referential identity of object instances.
     // It calls Object to compare values which is even better for testing than === strict.
+  });
+  test("Should return a correct output when given a valid input", () => {
+    const output = capitalizeString("croissant");
+    expect(output).not.toBe("Pain Au Chocolat");
+  });
+});
+
+describe("testing correct food type", () => {
+  test("Should be junk", () => {
+    const junkFood = [
+      { img: "🍕", name: "pizza", foodType: FoodType.JUNK, rating: 10 },
+      { img: "🍔", name: "burger", foodType: FoodType.JUNK, rating: 7 },
+      { img: "🍟", name: "fries", foodType: FoodType.JUNK, rating: 3 },
+      { img: "🌭", name: "hot dog", foodType: FoodType.JUNK, rating: 3 },
+    ];
+    const inputStock = foodStock;
+    const foodType = "junk" as FoodType;
+    const output = filterFoodByType(foodType, inputStock);
+    expect(output).toEqual(junkFood);
+  });
+
+  test("Should be healthy", () => {
+    const healthyFood = [
+      { img: "🥗", name: "salad", foodType: FoodType.HEALTHY, rating: 4 },
+      { img: "🥙", name: "pita", foodType: FoodType.HEALTHY, rating: 6 },
+      { img: "🥪", name: "sandwich", foodType: FoodType.HEALTHY, rating: 2 },
+      { img: "🌯", name: "burrito", foodType: FoodType.HEALTHY, rating: 9 },
+      { img: "🥣", name: "soup", foodType: FoodType.HEALTHY, rating: 1 },
+    ];
+    const inputStock = foodStock;
+    const foodType = "healthy" as FoodType;
+    const output = filterFoodByType(foodType, inputStock);
+    expect(output).toEqual(healthyFood);
   });
 });
