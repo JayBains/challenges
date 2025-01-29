da987f5835d5d27600966d1ec42c3621ff90a8bf
JayBains <jayarjunbains@gmail.com>
2025-01-29 12:09:15 +0000

    Fixed bug with multiple entries added to ul

--- ts/todo-list/src/main.ts
+++ ts/todo-list/src/main.ts
@@ -1,10 +1,12 @@
 import "../styles/styles.scss";
+// import { taskObj } from "./object.ts";
 
 let taskCount = document.querySelector<HTMLParagraphElement>("#counter");
 let addBtn = document.querySelector<HTMLButtonElement>("#add-button");
 let input = document.querySelector<HTMLInputElement>("#new-task");
 let ul = document.querySelector<HTMLUListElement>("#the-prophecy");
 
+// const taskArr: taskObj[] = [];
 const taskArr: string[] = [];
 
 function setupCounter(taskCount: HTMLParagraphElement) {
@@ -23,18 +25,14 @@ function setupCounter(taskCount: HTMLParagraphElement) {
     let newAddition = input.value;
     if (newAddition.length !== 0) {
       taskArr.push(newAddition);
-    }
-
-    for (let i = 0; i < taskArr.length; i++) {
       let li = document.createElement("li");
-      li.textContent = taskArr[i];
+      li.textContent = newAddition;
       ul.appendChild(li);
+      input.value = "";
+      setCounter(counter + 1);
     }
-    setCounter(counter + 1);
-    input.value = "";
     console.log(taskArr);
   });
-
   setCounter(0);
 }
 
