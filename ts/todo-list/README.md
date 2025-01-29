ae54291ae24a552357bff5c138502dddeb772f79
JayBains <jayarjunbains@gmail.com>
2025-01-29 13:13:53 +0000

    Added delete me - added BEM - updated readme

--- ts/todo-list/README.md
+++ ts/todo-list/README.md
@@ -1,3 +1,21 @@
 # To-Do List
 
 This is a test to demonstrate the skills acquired since starting the Nology Course
+
+Objective: Create a to-do list
+
+### Use
+
+- HTML
+- SCSS (BEM!)
+- TS
+
+### Functionality
+
+- Add new tasks
+- Delete (complete) tasks
+
+### Bonus
+
+- Greeting on the page based on the time of the day (morning, afternoon, evening etc)
+- Welcome message or random fact (or other) generated from an API call
--- ts/todo-list/src/main.ts
+++ ts/todo-list/src/main.ts
@@ -1,33 +1,51 @@
 import "../styles/styles.scss";
 
-let taskCount = document.querySelector<HTMLParagraphElement>("#counter");
-let addBtn = document.querySelector<HTMLButtonElement>("#add-button");
-let input = document.querySelector<HTMLInputElement>("#new-task");
-let ul = document.querySelector<HTMLUListElement>("#the-prophecy");
+let taskCount = document.querySelector<HTMLParagraphElement>(".counter");
+let addBtn = document.querySelector<HTMLButtonElement>(".card__add-button");
+let input = document.querySelector<HTMLInputElement>(".card__new-task");
+let ul = document.querySelector<HTMLUListElement>(".prophecy");
 
 if (!taskCount || !addBtn || !input || !ul) {
   throw new Error("missing elm");
 }
-const taskArr: string[] = [];
 
 addBtn.addEventListener("click", (event) => {
   createli(taskCount);
   return event;
 });
 
+const taskArr: string[] = [];
+
 function createli(taskCount: HTMLParagraphElement) {
   if (!input || !ul) {
     throw new Error("input, ul");
   }
 
-  let newAddition = input.value;
-  if (newAddition.length !== 0) {
-    taskArr.push(newAddition);
+  let text = input.value;
+  if (text.length > 2) {
+    taskArr.push(text);
     taskCount.textContent = `Tasks: ${taskArr.length}`;
+
     let li = document.createElement("li");
-    li.textContent = newAddition;
+    let delBtn = Object.assign(document.createElement("button"), {
+      className: "prophecy__delete",
+    });
+    delBtn.appendChild(document.createTextNode("Delete"));
+
+    li.textContent = text;
+    li.appendChild(delBtn);
     ul.appendChild(li);
+
     input.value = "";
+
+    delBtn.addEventListener("click", (event) => {
+      const index = taskArr.findIndex((item) => item === text);
+      taskArr.splice(index, 1);
+      ul.removeChild(li);
+      taskCount.textContent = `Tasks: ${taskArr.length}`;
+      console.log(index, taskArr);
+      return event;
+    });
   }
   console.log(taskArr);
 }
