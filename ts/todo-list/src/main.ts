d04632a24f772213925c36d0dec5ddede241f103
JayBains <jayarjunbains@gmail.com>
2025-01-29 12:25:37 +0000

    Simplified event listener

--- ts/todo-list/src/main.ts
+++ ts/todo-list/src/main.ts
@@ -1,31 +1,33 @@
 import "../styles/styles.scss";
-// import { taskObj } from "./object.ts";
 
 let taskCount = document.querySelector<HTMLParagraphElement>("#counter");
 let addBtn = document.querySelector<HTMLButtonElement>("#add-button");
 let input = document.querySelector<HTMLInputElement>("#new-task");
 let ul = document.querySelector<HTMLUListElement>("#the-prophecy");
 
-// const taskArr: taskObj[] = [];
+if (!taskCount || !addBtn || !input || !ul) {
+  throw new Error("missing elm");
+}
 const taskArr: string[] = [];
 
-function setupList(taskCount: HTMLParagraphElement) {
-  if (!addBtn || !input || !ul) {
-    throw new Error("missing elm");
+addBtn.addEventListener("click", (event) => {
+  createli(taskCount);
+  return event;
+});
+
+function createli(taskCount: HTMLParagraphElement) {
+  if (!input || !ul) {
+    throw new Error("input, ul");
   }
 
-  addBtn.addEventListener("click", () => {
-    let newAddition = input.value;
-    if (newAddition.length !== 0) {
-      taskArr.push(newAddition);
-      taskCount.textContent = `Tasks: ${taskArr.length}`;
-      let li = document.createElement("li");
-      li.textContent = newAddition;
-      ul.appendChild(li);
-      input.value = "";
-    }
-    console.log(taskArr);
-  });
+  let newAddition = input.value;
+  if (newAddition.length !== 0) {
+    taskArr.push(newAddition);
+    taskCount.textContent = `Tasks: ${taskArr.length}`;
+    let li = document.createElement("li");
+    li.textContent = newAddition;
+    ul.appendChild(li);
+    input.value = "";
+  }
+  console.log(taskArr);
 }
-
-setupList(taskCount!);
