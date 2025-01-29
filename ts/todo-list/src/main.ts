2e5144c994a89edc9f1719e2db240896cedee84e
JayBains <jayarjunbains@gmail.com>
2025-01-29 12:12:14 +0000

    Removed redundant code

--- ts/todo-list/src/main.ts
+++ ts/todo-list/src/main.ts
@@ -9,31 +9,23 @@ let ul = document.querySelector<HTMLUListElement>("#the-prophecy");
 // const taskArr: taskObj[] = [];
 const taskArr: string[] = [];
 
-function setupCounter(taskCount: HTMLParagraphElement) {
+function setupList(taskCount: HTMLParagraphElement) {
   if (!addBtn || !input || !ul) {
     throw new Error("missing elm");
   }
-
-  let counter = 0;
-
-  const setCounter = (count: number) => {
-    counter = count;
-    taskCount.textContent = `Tasks: ${counter}`;
-  };
 
   addBtn.addEventListener("click", () => {
     let newAddition = input.value;
     if (newAddition.length !== 0) {
       taskArr.push(newAddition);
+      taskCount.textContent = `Tasks: ${taskArr.length}`;
       let li = document.createElement("li");
       li.textContent = newAddition;
       ul.appendChild(li);
       input.value = "";
-      setCounter(counter + 1);
     }
     console.log(taskArr);
   });
-  setCounter(0);
 }
 
-setupCounter(taskCount!);
+setupList(taskCount!);
